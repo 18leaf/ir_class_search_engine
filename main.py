024b6b7b5ef9f6e78138fd1dbba2f3ca72ee0937
18leaf <18leaf@gmail.com>
2026-02-20 17:00:17 -0500

    wired in dataloader + tested

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from collections import defaultdict
+from dataloader import DataLoader
 from pathlib import Path
-
-def load_data(data_dir: Path):
-    
-
 
 
 def main():
     print("Hello from a2!")
+    dl = DataLoader(Path("./data"))
+    # begin loading data into Documents
+    dl.start()
 
 
 if __name__ == "__main__":
